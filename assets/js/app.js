37313c39920d72d46955548e44c3897586525cb0
Danuartha <idadanuartha42@smk.belajar.id>
2022-08-31 10:03:56 +0800

    menyelesaikan semua section dan menambahkan animasi ketika discroll menggunakan aos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Swiperjs script
 var swiper = new Swiper(".mySwiper", {
     cssMode: true,
     slidesPerView: 3,
@@ -8,10 +9,25 @@ var swiper = new Swiper(".mySwiper", {
       },
     mousewheel: true,
     keyboard: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
 });
 
+// Animate On Scroll Script
+AOS.init();
+
+
+// Add class scroll to navbar when window scrolling
 const navbar = document.querySelector('.navbar')
-// let lastScrollY = window.scrollY
 
 window.addEventListener('scroll', function() {
     if(window.scrollY > 80) {
